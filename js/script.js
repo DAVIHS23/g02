7111701620501ac3420e4fcd76caa3b70d20e332
Cedric Gisler <cedric.gisler@stud.hslu.ch>
2023-11-16 17:42:05 +0100

    Richtige Daten integrieren und Charts anpassen

--- js/script.js
+++ js/script.js
@@ -1,194 +1,161 @@
 // Pfad zur CSV-Datei
-const csvFilePath = 'data/data.csv';
+const csvFilePath = 'data/davi_data.csv';
 
 // Laden der CSV-Datei und dann Ausführung des angegebenen Codes
 d3.csv(csvFilePath).then(data => {
-    // Datenkonvertierung und Sortierung (wie zuvor geschrieben)
+  // Datenkonvertierung und Sortierung (wie zuvor geschrieben)
 
-    // Nach der Datenkonvertierung, erstelle die Ranglisten
-    createRanking('#esg-table', data.sort((a, b) => b.esg_score - a.esg_score), 'esg_score');
-    createRanking('#value-table', data.sort((a, b) => b.value - a.value), 'value');
+  // Nach der Datenkonvertierung, erstelle die Ranglisten
+  createRanking('#esg-table', data.sort((a, b) => b.esg_score - a.esg_score), 'esg_score');
+  createRanking('#market_capitalization-table', data.sort((a, b) => b.market_capitalization - a.market_capitalization), 'market_capitalization');
 
-    // Checkboxen hinzufügen
-    addIndustryCheckboxes(data, 'industry');
 
-    // Nun erstelle das Streudiagramm, mit Übergabe des entsprechenden Eigenschaftsnamens für die Industrie
-    createScatterplot('#scatterplot', data, 'esg_score', 'value', 'industry');  // Ersetze 'industry' mit deinem tatsächlichen Eigenschaftsnamen für die Industrie, falls er anders ist
+  // Erstellen des Streudiagramms mit CheckboxFen
+  createScatterplotWithCheckboxes('#scatterplot', data, 'esg_score', 'market_capitalization', 'industry');
 
-    // Bereiten Sie die Daten für das Donut-Diagramm vor
-    const preparedData = prepareDonutData(data, 'industry', 'employee_sizes');
-    createDonutChart('#donut-plot', preparedData);
+  // Erstellen des Donut-Diagramms
+  const preparedData = prepareDonutData(data, 'industry', 'full_time_employees');
+  createDonutChart('#donut-plot', preparedData);
 });
 
 // Funktion zur Erstellung der Rangliste in einem HTML-Element
 function createRanking(selector, data, columnName) {
-    // Zuerst das vorhandene Element leeren, um Duplikate zu vermeiden
-    d3.select(selector).html('');
+  // Zuerst das vorhandene Element leeren, um Duplikate zu vermeiden
+  d3.select(selector).html('');
 
-    // Erstellen der Tabelle
-    const table = d3.select(selector).append('table');
-    const headers = ['company', columnName, 'industry']; 
-    table.append('thead').append('tr').selectAll('th')
-        .data(headers)
-        .enter().append('th')
-        .text(d => d.charAt(0).toUpperCase() + d.slice(1).replace('_', ' '));  // Formatierung des Headers, um ihn leserlich zu machen
+  // Erstellen der Tabelle
+  const table = d3.select(selector).append('table');
+  const headers = ['company_name', columnName, 'industry'];
+  table.append('thead').append('tr').selectAll('th')
+    .data(headers)
+    .enter().append('th')
+    .text(d => d.charAt(0).toUpperCase() + d.slice(1).replace('_', ' '));  // Formatierung des Headers, um ihn leserlich zu machen
 
-    const rows = table.append('tbody').selectAll('tr')
-        .data(data)
-        .enter().append('tr');
+  const rows = table.append('tbody').selectAll('tr')
+    .data(data)
+    .enter().append('tr');
 
-    // Stellt sicher, dass die Daten korrekt den Zellen zugeordnet werden
-    rows.selectAll('td')
-        .data(row => [row.company, row[columnName], row.industry])  
-        .enter().append('td')
-        .text(d => typeof d === 'number' ? d.toLocaleString() : d);  // Zahlen werden für eine schönere Darstellung formatiert
+  // Stellt sicher, dass die Daten korrekt den Zellen zugeordnet werden
+  rows.selectAll('td')
+    .data(row => [row.company_name, row[columnName], row.industry])
+    .enter().append('td')
+    .text(d => typeof d === 'number' ? d.toLocaleString() : d);  // Zahlen werden für eine schönere Darstellung formatiert
 }
 
-// Funktion zur Erstellung eines Streudiagramms
-function createScatterplot(selector, data, xProp, yProp, industryProp) {
-    // Festlegen der Dimensionen und Abstände des Diagramms
-    const margin = { top: 20, right: 150, bottom: 70, left: 80 },
-          width = 920 - margin.left - margin.right,
-          height = 600 - margin.top - margin.bottom;
+function createScatterplotWithCheckboxes(selector, data, xProp, yProp, industryProp) {
+  // Erstellen des SVG-Elements für den Scatterplot
+  const margin = { top: 20, right: 150, bottom: 70, left: 80 },
+    width = 920 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
-    // Auswahl des SVG-Containers und Festlegen seiner Dimensionen
-    svg = d3.select(selector)
-                  .append("svg")
-                  .attr("width", width + margin.left + margin.right)
-                  .attr("height", height + margin.top + margin.bottom)
-                  .append("g")
-                  .attr("transform", `translate(${margin.left},${margin.top})`);
+  const svg = d3.select(selector)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    // Erstellen einer Farbskala für verschiedene Industrien
-    const color = d3.scaleOrdinal(d3.schemeCategory10)
-                    .domain(data.map(d => d[industryProp]));
+  // Erstellen des Scatterplots
+  createScatterplot(svg, data, width, height, xProp, yProp, industryProp);
 
-    // X-Achsen-Skala definieren
-    const x = d3.scaleLinear()
-                .domain(d3.extent(data, d => d[xProp]))
-                .range([0, width]);
-
-    // X-Achse im SVG zeichnen
-    svg.append("g")
-       .attr("transform", `translate(0,${height})`)
-       .call(d3.axisBottom(x));
-
-    // Beschriftung der X-Achse hinzufügen
-    svg.append("text")
-       .attr("text-anchor", "end")
-       .attr("x", width / 2 + margin.left)
-       .attr("y", height + margin.bottom / 2 )
-       .text(xProp.charAt(0).toUpperCase() + xProp.slice(1).replace('_', ' '));
-
-    // Y-Achsen-Skala definieren
-    const y = d3.scaleLinear()
-                .domain(d3.extent(data, d => d[yProp]))
-                .range([height, 0]);
-
-    // Y-Achse im SVG zeichnen
-    svg.append("g")
-       .call(d3.axisLeft(y));
-
-    // Beschriftung der Y-Achse hinzufügen
-    svg.append("text")
-       .attr("text-anchor", "end")
-       .attr("transform", "rotate(-90)")
-       .attr("y", -margin.left + 20)
-       .attr("x", -height / 2)
-       .text(yProp.charAt(0).toUpperCase() + yProp.slice(1).replace('_', ' '));
-
-    // Punkte (Datenpunkte) zum Streudiagramm hinzufügen
-    svg.append('g')
-       .selectAll("dot")
-       .data(data)
-       .enter()
-       .append("circle")
-       .attr("cx", d => x(d[xProp]))
-       .attr("cy", d => y(d[yProp]))
-       .attr("r", 5)
-       .style("fill", d => color(d[industryProp]));
-
-    // Legende für die Farbkodierung erstellen
-    const legend = svg.append("g")
-                      .attr("transform", `translate(${width + margin.right / 4}, 20)`);
-
-    // Legendenpunkte hinzufügen
-    const legendItem = legend.selectAll(".legend-item")
-                             .data(color.domain())
-                             .enter().append("g")
-                             .attr("class", "legend-item")
-                             .attr("transform", (d, i) => `translate(0, ${i * 20})`);
-
-    // Legendenfarbkästchen zeichnen
-    legendItem.append("rect")
-              .attr("x", 0)
-              .attr("width", 18)
-              .attr("height", 18)
-              .style("fill", color);
-
-    // Textbeschriftungen zur Legende hinzufügen
-    legendItem.append("text")
-              .attr("x", 22)
-              .attr("y", 9)
-              .attr("dy", "0.35em")
-              .text(d => d);
+  // Hinzufügen der Checkboxen
+  addIndustryCheckboxes(data, industryProp, svg);
 }
 
-// Höhere Ordnungsfunktion, die den Event-Handler für die Checkboxen generiert
-function createUpdateScatterplot(industryProp) {
-    // Diese zurückgegebene Funktion wird als Event-Handler für die Checkboxen verwendet
-    return function updateScatterplot() {
-      // Überprüfen, welche Checkboxen aktiviert sind
+function createScatterplot(svg, data, width, height, xProp, yProp, industryProp) {
+  // Erstellen einer Farbskala für verschiedene Industrien
+  const color = d3.scaleOrdinal(d3.schemeCategory10)
+    .domain(data.map(d => d[industryProp]));
+
+  // X-Achsen-Skala definieren
+  const x = d3.scaleLinear()
+    .domain(d3.extent(data, d => +d[xProp]))
+    .range([0, width]);
+
+  // Y-Achsen-Skala definieren
+  const y = d3.scaleSqrt()
+    .domain(d3.extent(data, d => +d[yProp]))
+    .range([height, 0]);
+
+  // Erstellen der Achsen
+  svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x));
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Hinzufügen von Punkten
+  svg.selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => x(+d[xProp]))
+    .attr("cy", d => y(+d[yProp]))
+    .attr("r", 5)
+    .style("fill", d => color(d[industryProp]));
+
+  // Hinzufügen der Legende
+  addLegendScatterplot(svg, data, color, width);
+}
+
+function addIndustryCheckboxes(data, industryProp, svg) {
+  const industries = [...new Set(data.map(d => d[industryProp]))];
+  const checkboxContainer = d3.select("#checkbox-container");
+
+  industries.forEach((industry, index) => {
+    const checkboxId = `checkbox-${index}`;
+    const checkboxGroup = checkboxContainer.append("div")
+      .attr("class", "checkbox-group");
+
+    checkboxGroup.append("input")
+      .attr("type", "checkbox")
+      .attr("class", "industry-checkbox")
+      .attr("id", checkboxId)
+      .attr("value", industry)
+      .property("checked", true);
+
+    checkboxGroup.append("label")
+      .attr("class", "checkbox-label")
+      .attr("for", checkboxId)
+      .text(industry);
+
+    d3.select(`#${checkboxId}`).on("change", function () {
       const checkedIndustries = d3.selectAll('.industry-checkbox:checked').nodes()
-                                     .map(node => node.value);
-  
-      // Datenpunkte ein-/ausblenden basierend auf der Auswahl
+        .map(node => node.value);
       svg.selectAll("circle")
-           .style("display", d => checkedIndustries.includes(d[industryProp]) ? null : "none");
-    };
-  }
-
-
-// Funktion zum Hinzufügen der Checkboxen
-function addIndustryCheckboxes(data, industryProp) {
-    // Eindeutige Branchen extrahieren
-    const industries = [...new Set(data.map(d => d[industryProp]))];
-  
-    // Container für Checkboxen
-    const checkboxContainer = d3.select("#checkbox-container");
-  
-    // Event-Handler-Funktion generieren, die Zugriff auf das richtige 'industryProp' hat
-    const updateScatterplotWithIndustryProp = createUpdateScatterplot(industryProp);
-  
-    // Checkboxen erstellen
-    industries.forEach(industry => {
-      // Gruppe für jede Checkbox und ihr Label
-      const checkboxGroup = checkboxContainer.append("div")
-                             .attr("class", "checkbox-group");
-  
-      const checkbox = checkboxGroup.append("input")
-                              .attr("type", "checkbox")
-                              .attr("class", "industry-checkbox")
-                              .attr("value", industry)
-                              .attr("checked", true); // Standardmäßig alle aktiviert
-  
-      checkboxGroup.append("label")
-                   .attr("class", "checkbox-label")
-                   .attr("for", industry) // Damit das Label richtig mit der Checkbox verknüpft ist
-                   .text(industry);
-  
-      // Event-Listener für Änderungen hinzufügen, mit der spezifischen Funktion
-      checkbox.on("change", updateScatterplotWithIndustryProp);
+        .style("display", d => checkedIndustries.includes(d[industryProp]) ? null : "none");
     });
-  }
-  
+  });
+}
+
+function addLegendScatterplot(svg, data, color, width) {
+  const industries = [...new Set(data.map(d => d.industry))];
+  const legend = svg.append("g")
+    .attr("class", "legend")
+    .attr("transform", `translate(${width + 20}, 30)`);
+
+  industries.forEach((industry, index) => {
+    legend.append("circle")
+      .attr("cx", 0)
+      .attr("cy", index * 20)
+      .attr("r", 5)
+      .style("fill", color(industry));
+
+    legend.append("text")
+      .attr("x", 10)
+      .attr("y", index * 20)
+      .text(industry)
+      .style("font-size", "10px")
+      .attr("alignment-baseline", "middle");
+  });
+}
 
 // Funktion zum Vorbereiten der Daten für das Donut-Diagramm
 function prepareDonutData(data, industryProp, employeeSizeProp) {
   // Verwenden von d3.rollup, um die Daten zu aggregieren
-  const industryCounts = d3.rollup(data, 
-                                   v => d3.sum(v, d => +d[employeeSizeProp]), // Summiere das 'employeeSizeProp' für jede Gruppe
-                                   d => d[industryProp]); // Gruppierung nach 'industryProp'
+  const industryCounts = d3.rollup(data,
+    v => d3.sum(v, d => +d[employeeSizeProp]), // Summiere das 'employeeSizeProp' für jede Gruppe
+    d => d[industryProp]); // Gruppierung nach 'industryProp'
 
   // Konvertieren der Map zurück in ein für d3.pie passendes Array
   const industryArray = Array.from(industryCounts, ([key, value]) => ({ key, value }));
@@ -199,80 +166,58 @@ function prepareDonutData(data, industryProp, employeeSizeProp) {
 function createDonutChart(selector, data) {
   // Größe und Marge des Diagramms festlegen
   const width = 800, // Breiter, um Platz für Beschriftungen zu schaffen
-        height = 550,
-        margin = 100; // Größere Marge für Beschriftungen
+    height = 550,
+    margin = 100; // Größere Marge für Beschriftungen
   const radius = Math.min(width, height) / 2 - margin;
 
   // SVG-Element erstellen
   const svg = d3.select(selector)
-                .append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                .append("g")
-                .attr("transform", `translate(${width / 2}, ${height / 2})`);
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
   // Die Farbskala definieren
   const color = d3.scaleOrdinal(d3.schemeCategory10);
 
   // Daten für das Diagramm vorbereiten
   const pie = d3.pie()
-                .value(d => d.value)
-                .sort(null); // um sicherzustellen, dass die Reihenfolge der Daten erhalten bleibt
+    .value(d => d.value)
+    .sort(null); // um sicherzustellen, dass die Reihenfolge der Daten erhalten bleibt
   const dataReady = pie(data);
 
   // Der Bogen-Generator für das Donut-Diagramm
   const arc = d3.arc()
-                .innerRadius(radius * 0.5) // Für den Donut-Effekt
-                .outerRadius(radius);
+    .innerRadius(radius * 0.5) // Für den Donut-Effekt
+    .outerRadius(radius);
 
   // Die Segmente des Donut-Diagramms erstellen
   svg.selectAll('allSlices')
-     .data(dataReady)
-     .enter()
-     .append('path')
-     .attr('d', arc)
-     .attr('fill', d => color(d.data.key))
-     .attr("stroke", "white")
-     .style("stroke-width", "2px")
-     .style("opacity", 0.7);
+    .data(dataReady)
+    .enter()
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', d => color(d.data.key))
+    .attr("stroke", "white")
+    .style("stroke-width", "2px")
 
-  // Der Bogen-Generator für die Labels
-  const labelArc = d3.arc()
-                     .innerRadius(radius * 0.85)
-                     .outerRadius(radius * 0.85);
+  // Legende erstellen
+  const legendContainer = d3.select("#donut-legend");
 
-  // Polylinien für die Labels hinzufügen
-  svg.selectAll('allPolylines')
-     .data(dataReady)
-     .enter()
-     .append('polyline')
-     .attr('points', d => {
-         const posA = arc.centroid(d); // Linienanfang auf dem Bogen
-         const posB = labelArc.centroid(d); // Linienmitte
-         const posC = labelArc.centroid(d); // Linienende
-         posC[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1); // Horizontaler Abstand
-         return [posA, posB, posC];
-     })
-     .style('fill', 'none')
-     .style('stroke', 'grey')
-     .style('stroke-width', '1px');
+  data.forEach((d, i) => {
+    const legendItem = legendContainer.append("div")
+      .style("display", "flex")
+      .style("align-items", "center")
+      .style("margin-bottom", "5px");
 
-  // Beschriftungen hinzufügen
-  svg.selectAll('allLabels')
-     .data(dataReady)
-     .enter()
-     .append('text')
-     .text(d => `${d.data.key}: ${d.data.value}`)
-     .attr('transform', d => {
-         const pos = labelArc.centroid(d);
-         pos[0] = radius * (midAngle(d) < Math.PI ? 1 : -1); // Horizontaler Abstand
-         return `translate(${pos})`;
-     })
-     .style('text-anchor', d => midAngle(d) < Math.PI ? 'start' : 'end')
-     .style('font-size', '12px');
+    legendItem.append("div")
+      .style("width", "20px")
+      .style("height", "20px")
+      .style("background-color", color(d.key))
+      .style("margin-right", "10px");
 
-  // Hilfsfunktion zur Bestimmung der Seiten (links/rechts) für die Textbeschriftung
-  function midAngle(d) {
-      return d.startAngle + (d.endAngle - d.startAngle) / 2;
-  }
+    legendItem.append("div")
+      .text(`${d.key}: ${d.value}`);
+  });
 }
