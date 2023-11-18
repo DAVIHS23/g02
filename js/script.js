9d8b5a287a200fb55504c369ea7323b1453ec890
Cedric Gisler <cedric.gisler@stud.hslu.ch>
2023-11-18 14:16:06 +0100

    Hinzufügen von Boxplots, blackwhite Scatter

--- js/script.js
+++ js/script.js
@@ -1,22 +1,86 @@
 // Pfad zur CSV-Datei
 const csvFilePath = 'data/davi_data.csv';
 
+
 // Laden der CSV-Datei und dann Ausführung des angegebenen Codes
 d3.csv(csvFilePath).then(data => {
-  // Datenkonvertierung und Sortierung (wie zuvor geschrieben)
+  //Frist Scatterplot Black and white
+  createScatterplotblackandwhite("#scatterplot-first-blackandwhite", data, "esg_score", "market_capitalization");
 
   // Nach der Datenkonvertierung, erstelle die Ranglisten
   createRanking('#esg-table', data.sort((a, b) => b.esg_score - a.esg_score), 'esg_score');
   createRanking('#market_capitalization-table', data.sort((a, b) => b.market_capitalization - a.market_capitalization), 'market_capitalization');
 
-
   // Erstellen des Streudiagramms mit CheckboxFen
   createScatterplotWithCheckboxes('#scatterplot', data, 'esg_score', 'market_capitalization', 'industry');
+
+  // Erstelle die Boxplots (environment_score, governance_score, social_score)
+  createBoxplot('#boxplot-environment', data, 'environment_score');
+  createBoxplot('#boxplot-governance', data, 'governance_score');
+  createBoxplot('#boxplot-social', data, 'social_score');
 
   // Erstellen des Donut-Diagramms
   const preparedData = prepareDonutData(data, 'industry', 'full_time_employees');
   createDonutChart('#donut-plot', preparedData);
 });
+
+//create blackandwhite Scatterplot
+function createScatterplotblackandwhite(selector, data, xProp, yProp) {
+  const margin = { top: 20, right: 20, bottom: 60, left: 70 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+  const x = d3.scaleLinear()
+    .range([0, width])
+    .domain(d3.extent(data, d => +d[xProp]));
+
+  const y = d3.scaleSqrt()
+    .range([height, 0])
+    .domain(d3.extent(data, d => +d[yProp]));
+
+  const svg = d3.select(selector)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  // X-Achse
+  svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x));
+
+  // Y-Achse
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // X-Achsenlegende
+  svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width / 2 + margin.left)
+    .attr("y", height + margin.top + 20)
+    .text(xProp.replace(/_/g, ' ')); // Ersetzt Unterstriche durch Leerzeichen
+
+  // Y-Achsenlegende
+  svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left + 20)
+    .attr("x", -height / 2)
+    .text(yProp.replace(/_/g, ' ')); // Ersetzt Unterstriche durch Leerzeichen
+
+  // Datenpunkte hinzufügen
+  svg.selectAll(".dot")
+    .data(data)
+    .enter().append("circle")
+    .attr("class", "dot")
+    .attr("r", 3.5)
+    .attr("cx", d => x(d[xProp]))
+    .attr("cy", d => y(d[yProp]))
+    .style("fill", "black"); // Alle Punkte in Schwarz
+
+}
+
 
 // Funktion zur Erstellung der Rangliste in einem HTML-Element
 function createRanking(selector, data, columnName) {
@@ -67,6 +131,8 @@ function createScatterplot(svg, data, width, height, xProp, yProp, industryProp)
   const color = d3.scaleOrdinal(d3.schemeCategory10)
     .domain(data.map(d => d[industryProp]));
 
+  const margin = { top: 20, right: 20, bottom: 60, left: 70 }
+
   // X-Achsen-Skala definieren
   const x = d3.scaleLinear()
     .domain(d3.extent(data, d => +d[xProp]))
@@ -83,6 +149,21 @@ function createScatterplot(svg, data, width, height, xProp, yProp, industryProp)
     .call(d3.axisBottom(x));
   svg.append("g")
     .call(d3.axisLeft(y));
+
+  // X-Achsenlegende
+  svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width / 2 + margin.left)
+    .attr("y", height + margin.top + 20)
+    .text(xProp.replace(/_/g, ' ')); // Ersetzt Unterstriche durch Leerzeichen
+
+  // Y-Achsenlegende
+  svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left + 20)
+    .attr("x", -height / 2)
+    .text(yProp.replace(/_/g, ' ')); // Ersetzt Unterstriche durch Leerzeichen
 
   // Hinzufügen von Punkten
   svg.selectAll("dot")
@@ -221,3 +302,122 @@ function createDonutChart(selector, data) {
       .text(`${d.key}: ${d.value}`);
   });
 }
+
+function createBoxplot(selector, data, scoreType) {
+  // Definieren der Dimensionen und Margen des SVG-Canvas
+  let margin = { top: 10, right: 30, bottom: 90, left: 70 },
+    width = 800 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+  // Anhängen des SVG-Elements an das gewünschte Div-Element
+  let svg = d3.select(selector)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  // Gruppieren der Daten nach Industrie
+  let groupedData = d3.group(data, d => d.industry);
+
+  // Erstellen einer ordinalen Farbskala
+  let colorScale = d3.scaleOrdinal()
+    .domain(Array.from(groupedData.keys()))
+    .range(d3.schemeTableau10);
+
+  // Erstellen der X-Skala
+  let x = d3.scaleBand()
+    .range([0, width])
+    .domain(Array.from(groupedData.keys()))
+    .padding(0.2);
+
+  svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+  // Erstellen der Y-Skala
+  let y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => +d[scoreType])])
+    .range([height, 0])
+    .nice();
+
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Funktion zum Berechnen der Quartile, Whiskers und Ausreißer
+  function boxQuartilesAndWhiskers(d) {
+    let sorted = d.map(g => g[scoreType]).sort(d3.ascending);
+    let q1 = d3.quantile(sorted, .25);
+    let median = d3.quantile(sorted, .5);
+    let q3 = d3.quantile(sorted, .75);
+    let iqr = q3 - q1;
+    let min = Math.max(q1 - 1.5 * iqr, 0); // Verhindern, dass der Wert unter 0 fällt
+    let max = q3 + 1.5 * iqr;
+    let outliers = d.filter(g => g[scoreType] < min || g[scoreType] > max);
+    return { q1, median, q3, min, max, outliers };
+  }
+
+
+  // Zeichnen der Boxplots
+  groupedData.forEach((groupValues, groupName) => {
+    let { q1, median, q3, min, max, outliers } = boxQuartilesAndWhiskers(groupValues);
+    let boxWidth = 25;
+
+    // Box mit Farbskala
+    svg.append("rect")
+      .attr("x", x(groupName) - boxWidth / 2)
+      .attr("y", y(q3))
+      .attr("width", boxWidth)
+      .attr("height", y(q1) - y(q3))
+      .attr("stroke", "black")
+      .style("fill", colorScale(groupName));
+
+    // Median
+    svg.append("line")
+      .attr("x1", x(groupName) - boxWidth / 2)
+      .attr("x2", x(groupName) + boxWidth / 2)
+      .attr("y1", y(median))
+      .attr("y2", y(median))
+      .attr("stroke", "black");
+
+    /*
+  // Unterer Whisker
+  svg.append("line")
+    .attr("x1", x(groupName))
+    .attr("x2", x(groupName))
+    .attr("y1", y(min))
+    .attr("y2", y(q1))
+    .attr("stroke", "black");
+
+  // Oberer Whisker
+  svg.append("line")
+    .attr("x1", x(groupName))
+    .attr("x2", x(groupName))
+    .attr("y1", y(max))
+    .attr("y2", y(q3))
+    .attr("stroke", "black");
+*/
+
+    // Ausreißer
+    outliers.forEach(d => {
+      svg.append("circle")
+        .attr("cx", x(groupName))
+        .attr("cy", y(d[scoreType]))
+        .attr("r", 3)
+        .style("fill", "black");
+    });
+
+    // Titel zur Y-Achse
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 20)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "0.8em")
+      .style("text-anchor", "middle")
+      .text(scoreType.replace('_', ' ').toUpperCase());
+  });
+}
+
